5cb425171f5c4c707114d7ffab7104e27731813e
James Walters <33947683+iamjameswalters@users.noreply.github.com>
2021-11-11 20:56:52 -0500

    Refactored models a little

--- mealwise/inventory/models.py
+++ mealwise/inventory/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Ingredient(models.Model):
     name = models.CharField(max_length=50, unique=True)
-    quantity = models.IntegerField(max_length=3, default=0)
+    quantity = models.FloatField(default=0)
     unit = models.CharField(max_length=15)
     price_per_unit = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
 
@@ -20,7 +20,7 @@ class MenuItem(models.Model):
 class RecipeRequirement(models.Model):
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
-    quantity_required = models.FloatField(default=0)
+    quantity = models.FloatField(default=0)
 
     def __str__(self):
         return "{ing} requirement for {menu}".format(ing=self.ingredient.name, menu=self.menu_item.name)
