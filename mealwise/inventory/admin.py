43c6d06c73f476535f86f84778671224500609df
James Walters <33947683+iamjameswalters@users.noreply.github.com>
2021-11-11 21:01:45 -0500

    Registered models in admin interface

--- mealwise/inventory/admin.py
+++ mealwise/inventory/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.Ingredient)
+admin.site.register(models.MenuItem)
+admin.site.register(models.RecipeRequirement)
+admin.site.register(models.Purchase)
