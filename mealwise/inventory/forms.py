8d2bb56783c0ae3f26c1d666fcfa1acbed8cc97c
James Walters <33947683+iamjameswalters@users.noreply.github.com>
2021-11-29 17:21:55 -0500

    Fixed typo in forms.py and made initial migrations

--- mealwise/inventory/forms.py
+++ mealwise/inventory/forms.py
@@ -1,22 +1,22 @@
 from django import forms
 from . import models
 
-class MenuItemForm(forms.modelForm):
+class MenuItemForm(forms.ModelForm):
   class Meta:
     model = models.MenuItem
     fields = "__all__"
 
-class RecipeRequirementForm(forms.modelForm):
+class RecipeRequirementForm(forms.ModelForm):
   class Meta:
     model = models.RecipeRequirement
     fields = "__all__"
 
-class IngredientForm(forms.modelForm):
+class IngredientForm(forms.ModelForm):
   class Meta:
     model = models.Ingredient
     fields = "__all__"
 
-class PurchaseForm(forms.modelForm):
+class PurchaseForm(forms.ModelForm):
   class Meta:
     model = models.Purchase
     fields = "__all__"
