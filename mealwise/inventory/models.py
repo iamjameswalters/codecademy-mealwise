a2cebfffdb45e8ee57f6723bf72ab36a7075f08e
James Walters <33947683+iamjameswalters@users.noreply.github.com>
2021-12-02 17:42:51 -0500

    Cleaned up __str__ methods

--- mealwise/inventory/models.py
+++ mealwise/inventory/models.py
@@ -8,7 +8,7 @@ class Ingredient(models.Model):
     price_per_unit = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
 
     def __str__(self):
-        return "{} ingredient".format(self.name)
+        return "{name} ingredient, quantity: {quan} {unit}(s)".format(name=self.name, quan=self.quantity, unit=self.unit)
 
     def get_absolute_url(self):
         return "/ingredients"
@@ -18,7 +18,7 @@ class MenuItem(models.Model):
     price = models.DecimalField(max_digits=4, decimal_places=2, default=0.00)
 
     def __str__(self):
-        return "{}".format(self.name)
+        return "{item}: ${price}".format(name=self.name, price=self.price)
 
     def get_absolute_url(self):
         return "/menu"
@@ -35,7 +35,7 @@ class RecipeRequirement(models.Model):
     quantity = models.FloatField(default=0)
 
     def __str__(self):
-        return "{ing} requirement for {menu}".format(ing=self.ingredient.name, menu=self.menu_item.name)
+        return "Recipe: {menu} requires {quan} {unit}(s) of {ing}".format(ing=self.ingredient.name, menu=self.menu_item.name, quan=self.quantity, unit=self.ingredient.unit)
 
     def get_absolute_url(self):
         return "/recipes"
@@ -48,7 +48,7 @@ class Purchase(models.Model):
     time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return "Order for {}".format(self.menu_item.name)
+        return "Order for {item} at {time}".format(item=self.menu_item.name, time=self.time)
 
     def get_absolute_url(self):
         return "/purchases"
