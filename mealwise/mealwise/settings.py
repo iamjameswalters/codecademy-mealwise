8d5133c01b354da408ec9bd049ca88ac9d31291d
James Walters <33947683+iamjameswalters@users.noreply.github.com>
2021-12-02 17:55:31 -0500

    Set timezone to EST

--- mealwise/mealwise/settings.py
+++ mealwise/mealwise/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
@@ -127,5 +127,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
-
-USE_TZ = True
