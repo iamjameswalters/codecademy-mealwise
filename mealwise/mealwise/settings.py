03a526e3acc99010fc46d2e845f8a88f9cbafb33
James Walters <33947683+iamjameswalters@users.noreply.github.com>
2021-12-08 13:26:41 -0500

    Adds django-htmx middleware to project

--- mealwise/mealwise/settings.py
+++ mealwise/mealwise/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'inventory.apps.InventoryConfig'
+    'inventory.apps.InventoryConfig',
+    'django_htmx'
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_htmx.middleware.HtmxMiddleware'
 ]
 
 ROOT_URLCONF = 'mealwise.urls'
