a4df23ab5f8d010d3be92045ffbe6237e70bb588
James Walters <33947683+iamjameswalters@users.noreply.github.com>
2021-12-17 17:23:01 -0500

    Added django-bootstrap5

--- mealwise/mealwise/settings.py
+++ mealwise/mealwise/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'inventory.apps.InventoryConfig',
-    'django_htmx'
+    'django_htmx',
+    'django_bootstrap5'
 ]
 
 MIDDLEWARE = [
