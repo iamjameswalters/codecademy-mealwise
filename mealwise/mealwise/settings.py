f0172b19109404cc901b35ddf78eb561e87f158b
James Walters <33947683+iamjameswalters@users.noreply.github.com>
2022-02-09 13:27:45 -0500

    Changed where static files are collected

--- mealwise/mealwise/settings.py
+++ mealwise/mealwise/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'inventory/static'
+STATIC_ROOT = BASE_DIR / 'inventory/static/collected_assets'
 STATIC_URL = '/static/'
 
 # Default primary key field type
