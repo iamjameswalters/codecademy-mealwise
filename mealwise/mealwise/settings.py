a0c1318c43f02538efa18158264dce483b80e193
James Walters <33947683+iamjameswalters@users.noreply.github.com>
2023-03-17 18:00:46 -0400

    Removed whitenoise STATICFILES_STORAGE

--- mealwise/mealwise/settings.py
+++ mealwise/mealwise/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Default primary key field type
